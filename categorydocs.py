76133fa7cddf4315fa030b52a835f172a17f54fc
JesseHsiu <jesse801122@gmail.com>
2015-12-12 23:39:15 +0800

    update

--- categorydocs.py
+++ categorydocs.py
@@ -9,8 +9,25 @@ import codecs
 from types import *
 import multiprocessing as mp
 import collections
+import jieba
+import jieba.analyse
 
 allowCategory = [ u'社會', u'娛樂', u'政治', u'生活']
+
+# jieba add_word
+jieba.add_word('黄世铭')
+jieba.add_word('特侦组')
+jieba.add_word('黄色小鸭')
+jieba.add_word('大统')
+jieba.add_word('太阳花')
+jieba.add_word('服贸协定')
+jieba.add_word('服贸')
+jieba.add_word('波卡')
+jieba.add_word('台商')
+jieba.add_word('北捷')
+jieba.add_word('郑捷')
+jieba.add_word('瓦斯')
+jieba.add_word('气爆')
 
 class EBCawler(object):
 	def __init__(self):
@@ -89,6 +106,7 @@ def parseFile(filename, dirname, outputDir):
 			f.write(content_text)
 		f.closed
 
+
 def simplify(text):
  return opencc.convert(text, config='t2s.json')
 
@@ -97,9 +115,8 @@ def traditionalize(text):
 
 if __name__ == '__main__':
 	
-	if len(sys.argv) == 3:
+	if len(sys.argv) == 4:
 		# ======= Trainning News Category =======
-
 		# Depends on current news classifier
 		ebCawler = EBCawler()
 		# Warning!!! - Need web connection
@@ -113,23 +130,37 @@ if __name__ == '__main__':
 		# ======= Simplify All Original Docs =======
 		originDocs_Dir = sys.argv[1]
 		outputDocs_Dir = sys.argv[2]
+		outputWithCateDocs_Dir = sys.argv[3]
 		originDocs = OriginDocs(originDocs_Dir, outputDocs_Dir)
 		# originDocs.simplifyAllDoc();
-		
 
 		# ======= Category Original Docs and save to json file =======
-		dataDict = [[] for i in range(len(allowCategory))]
 		for x in os.listdir(outputDocs_Dir):
 			if x == ".DS_Store":
 				continue
 			content = None
 			with open(outputDocs_Dir + "/" + x) as f:
 				content = f.readline()
+			list = []
+			content_sentences = content.split('。')
+			for sentence in content_sentences:
+				if len(sentence) != 0:
+					list.extend(jieba.cut(sentence, cut_all=True))
 			result = grocery.predict(content)
 			print x
-			dataDict[int(result)].append(x[:-4])
-		with codecs.open("./catagoryResult.json", "w", "utf-8") as f:
-			json.dump(dataDict, f)
+			with codecs.open(outputWithCateDocs_Dir + "/" + x, "w", "utf-8") as f:
+				f.write(str(result))
+				f.write("\n")
+				for term in list:
+					# if the term word is less than 2, which is a single word, then ignore it
+					if len(term) < 2:
+						continue
+					else:
+						f.write(term)
+						f.write(',')
+				
+		# ======= Train 4 Category with 3 Publisher(LTN,CTS,APD) =======
+		
 
 	else:
-		print "Please use like 'python sim.py [originDocs_Dir] [outputDocs_Dir] [querySaveDir]'"
+		print "Please use like 'python sim.py [originDocs_Dir] [outputDocs_Dir] [With]'"
