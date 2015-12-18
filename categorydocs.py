e4cf20ccfad58435b40918633cdd9be80c31d7f2
JesseHsiu <jesse801122@gmail.com>
2015-12-18 17:05:06 +0800

    0.53215

--- categorydocs.py
+++ categorydocs.py
@@ -16,6 +16,8 @@ import jieba.posseg as pseg
 from scipy import stats
 from snownlp import sentiment
 from snownlp import SnowNLP
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Logging from gensim
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
@@ -119,7 +121,7 @@ class CorporaMgr(object):
 			if publisher_count == 3:
 				publisher_count = 0
 				cate_count+=1
-	def getClassifiyPublisherName(self, queryDoc, category):
+	def getClassifiyPublisherName(self, queryDoc, category, sentimentResult):
 		words = pseg.cut(simplify(removeCharacter(queryDoc)))
 		terms = []
 		allowFlag = ['v','vd','vn','vshi','vyou','vf','vx','vi','vl','vg','a','ad','an','ag','al','d']
@@ -129,30 +131,52 @@ class CorporaMgr(object):
 				# print word, 
 			# print('%s %s' % (word, flag))
 
-		totalDocs = 0
-		for x in xrange(0, len(self.index)) :
-			if x >= category*len(publisherList) and x < category*len(publisherList)+ len(publisherList):
-				totalDocs += len(self.index[x])
+		# totalDocs = 0
+		# for x in xrange(0, len(self.index)) :
+		# 	if x >= category*len(publisherList) and x < category*len(publisherList)+ len(publisherList):
+		# 		totalDocs += len(self.index[x])
 
-		normalizer = []
-		for x in xrange(0, len(self.index)) :
-			if x >= category*len(publisherList) and x < category*len(publisherList)+ len(publisherList):
-				normalizer.append(totalDocs/float(len(self.index[x])))
+		# normalizer = []
+		# for x in xrange(0, len(self.index)) :
+		# 	if x >= category*len(publisherList) and x < category*len(publisherList)+ len(publisherList):
+		# 		normalizer.append(totalDocs/float(len(self.index[x])))
 		# print normalizer
 
 
-		results = []
+		semti = 0
+		count = 0
+		for term in terms:
+			s = SnowNLP(term)
+			if s.sentiments == 0.5:
+				continue
+			semti += s.sentiments
+			count += 1
+
+		sentimentOfQuery = 0
+		
+		if count == 0:
+			sentimentOfQuery = 0
+		else:
+			sentimentOfQuery = semti/float(count)
+		
+
+		resultsSemi = []
+		resultsLike = []
 		count = 0
 		for x in xrange(0, len(self.corporaDict)) :
 			# print self.corporaDict[x].doc2bow(terms)
 			if x >= category*len(publisherList) and x < category*len(publisherList)+ len(publisherList):
-				result = self.index[x][self.corporaDict[x].doc2bow(terms)]
+
+				resultsSemi.append( 1 + sentimentResult[x].getPercentageOfValue(sentimentOfQuery))
+
+
+				resultLikeFromFile = self.index[x][self.corporaDict[x].doc2bow(terms)]
 				
 				# === ALL RESULT ===
-				list = []
-				results.append(list)
-				for rank in result:
-					list.append(rank[1])
+				# list = []
+				# results.append(list)
+				# for rank in result:
+					# list.append(rank[1])
 
 				
 				# ==== MIDDLE ===
@@ -164,18 +188,18 @@ class CorporaMgr(object):
 
 				# ==== FIRST VALUE ===
 				# if len(result) == 0:
-				# 	results.append(0)
+				# 	resultsLike.append(0)
 				# else:
-				# 	results.append(result[0][1])
+				# 	resultsLike.append(result[0][1])
 
 				# ==== AVG ===
-				# avg_score = []
-				# for rank in result:
-				# 	avg_score.append(rank[1])
-				# if len(avg_score) == 0:
-				# 	results.append(0)
-				# else:
-				# 	results.append(sum(avg_score) / float(len(avg_score)))
+				avg_score = []
+				for rank in resultLikeFromFile:
+					avg_score.append(rank[1])
+				if len(avg_score) == 0:
+					resultsLike.append(0)
+				else:
+					resultsLike.append(sum(avg_score) / float(len(avg_score)))
 				# count+=1
 
 		# minLength = 50
@@ -219,11 +243,12 @@ class CorporaMgr(object):
 		# 		lower = 2
 		# 	pValue.append(stats.ttest_ind(results[x],results[upper])[1] + stats.ttest_ind(results[x],results[lower])[1])
 
+		resultOfAll = []
+		for x in xrange(0,len(resultsSemi)):
+			resultOfAll.append( resultsSemi[x] * resultsLike[x] )
 
-
-
-		print finalOutput
-		return numToPublisher(finalOutput.index(max(finalOutput)))
+		print resultOfAll
+		return numToPublisher(resultOfAll.index(max(resultOfAll)))
 
 class DocCorpus(object):
 	def __init__(self, texts, dict):
@@ -266,6 +291,44 @@ class Docs(object):
 							yield [category, publisher, content]
 				else:
 					continue
+
+class SentimentMgr(object):
+	def __init__(self):
+		# self.min = 1
+		# self.max = 0
+		# self.count = 0
+		# self.sentiValue = 0
+		self.values = []
+	def addValue(self, sentiValue):
+		self.values.append(sentiValue)
+		# print self.values
+		
+		# if sentiValue > self.max:
+		# 	self.max = sentiValue
+
+		# if sentiValue < self.min:
+		# 	self.min = sentiValue
+
+		# self.count += 1
+		# self.sentiValue += sentiValue
+	def max(self):
+		return np.amax(self.values)
+	def min(self):
+		return np.amin(self.values)
+	def median(self):
+		return np.median(self.values)
+	def average(self):
+		return np.average(self.values)
+	def std(self):
+		return np.std(self.values)
+	def saveHistogram(self, fname):
+		plt.clf()
+		plt.hist(self.values, 100,range=[0, 1])
+		plt.savefig(fname)
+	def getPercentageOfValue(self, value):
+		valuesInArray = np.array(self.values)
+		return np.compress(((round(value,1)-0.1 <= valuesInArray) & (valuesInArray <= round(value,1)+0.1)),valuesInArray).size / float(len(self.values))
+
 
 ########## FUNCTIONS ##########
 
@@ -314,6 +377,15 @@ def categoryToNum(argument):
         u'娛樂':1,
         u'政治':2,
         u'生活':3,
+    }
+    return switcher.get(argument)
+
+def numToCategory(argument):
+    switcher = {
+        0:u'社會',
+        1:u'娛樂',
+        2:u'政治',
+        3:u'生活',
     }
     return switcher.get(argument)
 
@@ -410,63 +482,146 @@ if __name__ == '__main__':
 		corporaMgr.generateCategoryAndPublisher()
 
 
-		sentiments_FromSet = []
+		# sentiments_FromSet = []
 		# SENTIMENT TRAINNING FROM NTUSD
 		# sentiment.train('neg.txt', 'pos.txt')
 		# sentiment.save('sentiment.marshal')
 
+		sentimentResult = []
+
+		# ====== Sentiments ======
 		for cate in xrange(0, len(allowCategory)):
 			print "cate", cate
 			for publisher in xrange(0, len(publisherList)):
+				sentiMgr = SentimentMgr()
+				sentimentResult.append(sentiMgr)
 				print "publisher", publisher
-				count = 0
-				senti = 0.0
+				
 				for doc in Docs(outputWithCateDocs_Dir,cate,publisher):
-					
+					count = 0
+					semti = 0.0	
 					if len(doc[2]) == 0:
 						continue
-					count_inDoc = 0
-					senti_inDoc = 0.0
 					for term in doc[2]:
 						s = SnowNLP(unicode(term, 'utf-8'))
 						if s.sentiments == 0.5:
 							continue
-						senti_inDoc += s.sentiments
-						print term,s.sentiments
-						count_inDoc += 1
+						semti += s.sentiments
+						count += 1
+					if count == 0:
+						continue
+					else:
+						sentiMgr.addValue(semti/float(count))
+						# print "median: ",sentiMgr.median()
+						# , "min: ", sentiMgr.min, "avg:", sentiMgr.avgValue(), "-", semti/float(count)
 
-					senti += senti_inDoc/float(count_inDoc)
-					# print "s.sentiments:",s.sentiments
-					count += 1
-				print "semti / float(count):", semti / float(count)
-				sentiments_FromSet.append(semti / float(count))
+					# if count_inDoc == 0:
+						# continue
+					# else:
+						# print senti_inDoc,count_inDoc
+						# semti += float(senti_inDoc)/float(count_inDoc)
+						# count += 1
+				print "================="
+				print "===max: ",sentiMgr.max(), "min: ", sentiMgr.min(), "avg:", sentiMgr.average()
+				sentiMgr.saveHistogram('current.png')
+				# break
+				# sentiments_FromSet.append(sentiMgr.avgValue())
 
-		print sentiments_FromSet
+		publisher_count = 0
+		count = 0
+		cate_count = 0
 
+		for result in sentimentResult:
+			result.saveHistogram(str(cate_count) + "_" + str(publisher_count) + ".png")
+			publisher_count+=1
+			count+=1
+			if publisher_count == 3:
+				publisher_count = 0
+				cate_count+=1
+			print result.max(), result.min(), result.median(), result.average(), result.std()
+			# 0.935333489596 0.00875296241739 0.319083118846 0.326583518388 0.0610376482466
+			# 0.784000052087 0.0014028422927 0.321060214695 0.328754298799 0.0609401440388
+			# 0.878522504892 0.0615563217165 0.324475004439 0.330887137374 0.0555331749459
+			# 
+			# 0.935333489596 0.0106472864747 0.3697758035 0.379513786538 0.098874425074
+			# 0.812518360162 0.0300867666187 0.375512839139 0.379377350102 0.0912036821854
+			# 0.669268342313 0.147583789222 0.367846773649 0.373351085225 0.0677476942484
 
-		# 
+			# 0.915597328212 0.0622895622896 0.313632581676 0.321264363939 0.0557029996915
+			# 0.666666666667 0.0582134937822 0.320980445391 0.327295822889 0.057736354674
+			# 0.568727372665 0.168654524941 0.320292885567 0.326198792206 0.0507511380966
+
+			# 0.75 0.0568408790917 0.347004991076 0.354779423899 0.0674763535299
+			# 0.792439372976 0.0588539620499 0.353527820966 0.360996346848 0.0713863655732
+			# 0.823173454013 0.15818249969 0.344966478066 0.353326906681 0.0615560359763
+			
+		# print sentiments_FromSet
+
+		# allowCategory = [ u'社會', u'娛樂', u'政治', u'生活']
+		# publisherList = [ 'LTN', 'APD', 'CTS']
+		# 0.319, 0.3178, 0.327
+		# 0.367, 0.38
+		# [0.32658351838803207, 0.3287542987987323,  0.33088713737381503,
+		#  0.37951378653834134, 0.37937735010195844, 0.3733510852251421,
+		#  
+		#  
+		#  0.3212643639392646,  0.32729582288912906, 0.32619879220636183,
+		#  0.39268
+		#  
+		#  0.35477942389873096, 0.36099634684839993, 0.35332690668123784]
+		# sentiMgr = SentimentMgr()
+		# for fname in os.listdir(queryDir):
+		# 	if fname == ".DS_Store":
+		# 		continue
+		# 	with open(os.path.join(queryDir, fname)) as f:
+		# 		data = f.read().splitlines()
+		# 		nameOfdata = data[0]
+		# 		# print nameOfdata
+		# 		content = ""
+		# 		for x in xrange(2,len(data)):
+		# 			# print data[x]
+		# 			content += data[x]
+				
+		# 		s_total = 0
+		# 		count = 0
+		# 		for term in jieba.lcut(content):
+		# 			s = SnowNLP(term)
+		# 			if s.sentiments == 0.5:
+		# 				continue
+		# 			# print term,s.sentiments
+		# 		if grocery.predict(simplify(content)) == 2:
+		# 			s_total += s.sentiments
+		# 			count += 1
+		# 			sentiMgr.addValue(s_total/float(count))
+				# print simplify(content)
+				# print nameOfdata,s.sentiments
+
 
 		# ======= Predict And result.csv =======
-		# with open( "result.csv", "w") as outputfile:
-		# 	outputfile.write("NewsId,Agency")
-		# 	outputfile.write("\n")
-		# 	for fname in os.listdir(queryDir):
-		# 		if fname == ".DS_Store":
-		# 			continue
-		# 		with open(os.path.join(queryDir, fname)) as f:
-		# 			data = f.read().splitlines()
-		# 			nameOfdata = data[0]
-		# 			print nameOfdata
-		# 			content = ""
-		# 			for x in xrange(2,len(data)):
-		# 				# print data[x]
-		# 				content += data[x]
-		# 			# print content
-		# 			result = corporaMgr.getClassifiyPublisherName(content, grocery.predict(simplify(content)))
-		# 			outputfile.write(nameOfdata)
-		# 			outputfile.write(",")
-		# 			outputfile.write(result)
-		# 			outputfile.write("\n")
+		with open( "result.csv", "w") as outputfile:
+			outputfile.write("NewsId,Agency")
+			outputfile.write("\n")
+			for fname in os.listdir(queryDir):
+				if fname == ".DS_Store":
+					continue
+				with open(os.path.join(queryDir, fname)) as f:
+					data = f.read().splitlines()
+					nameOfdata = data[0]
+					print nameOfdata
+					content = ""
+					for x in xrange(2,len(data)):
+						# print data[x]
+						content += data[x]
+					# print content
+					result = 0
+					# if grocery.predict(simplify(content)) == 2:
+						
+					# else:
+					result = corporaMgr.getClassifiyPublisherName(content, grocery.predict(simplify(content)),sentimentResult)
+					outputfile.write(nameOfdata)
+					outputfile.write(",")
+					outputfile.write(result)
+					outputfile.write("\n")
 
 	else:
 		print "Please use like 'python sim.py [originDocs_Dir] [outputDocs_Dir] [WithCatagoryAndPublisher] [train.csv]'"
